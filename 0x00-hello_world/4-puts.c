fcb40e2c18cef52f18e4e5eccbef0c36499f612b
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2022-12-08 08:37:24 -0800

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,6 @@
 * main - entry
 * Return: Always 0
 */
-int main (void)
-{
+int main (void) {
     puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}
+    return (0); }
