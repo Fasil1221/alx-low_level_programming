9599a6f8de64b322bc9297fd5628756a631c812e
fasil1221 <fasiltassew1@gmail.com>
2022-12-08 09:08:27 -0800

    is this it

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
-* main - entry
-* Return: Always 0
-*/
-int main (void){
-puts("\"Programming is like building a multilingual puzzle"); 
-return (0); }
+ * main - entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"putsProgramming is like building a multilingual puzzle");
+	return (0);
+}
