5179b8d3f9087858e2140014bf812e78817f0847
fasil1221 <fasiltassew1@gmail.com>
2022-12-08 08:19:30 -0800

    first attempt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+
+* main - entry
+
+* Return: Always 0
+
+*/
+
+int main (void)
+
+{
+
+    puts("\"Programming is like building a multilingual puzzle");
+
+     return (0);
+
+
+
+}
