10f314a7bbc4977a6893afbc73f97bfc2a14ae67
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 12:35:53 -0800

    this is it

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - lower case
+ * Return: always 0
+ */
+int main(void)
+{	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+	putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
