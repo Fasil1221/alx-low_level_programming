8bb486987a0654daf8fba0613097fe9ed05cc32b
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 14:42:09 -0800

    this must bre it

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - combputchar(n);ination of single digits
+ * Return: always 0
+ */
+	int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	if (n != 57)
+{
+	putchar(',');
+	putchar(' ');
+										}
+	putchar('\n');
+
+	return (0);
+}
