6da42b16b479a652a836e42d028b9ba588d9b6e1
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 14:23:16 -0800

    this is it by micheal jackson

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints base 16 numbers
+ * Return: always 0
+ */
+	int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+
+	for (la = 'a'; la <= 'f'; la++)
+	putchar(la);
+	putchar('n');
+
+	return (0);
+}
