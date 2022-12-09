96d4e3757682e4d5de8cb915c2699162bf27dd28
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 14:06:27 -0800

    this is it

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints reverse alphabet
+ * Return: always 0
+ */
+	int main(void)
+{
+	char le;
+
+	for (le = 'z'; le >= 'a'; le--)
+		putchar(le);
+	putchar('\n')
+
+	return (0);
+}
