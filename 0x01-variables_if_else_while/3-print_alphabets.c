8f04a6ca2d17e2c3348291afe483193bcdf3dfa7
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 12:57:44 -0800

    this is it

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - lower case and upper case followed by new line
+ * Return: always 0
+ */
+	int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
