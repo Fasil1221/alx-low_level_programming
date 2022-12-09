8a19bae234fba2df0978b49e3cfc0c71685f8ffd
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 13:18:44 -0800

    this is it

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - lower case followed by new line
+ * Return: always 0
+ */
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; ch <= 'z'; ch++)
+	{
+	if (la != 'e' && la != 'q')
+	putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
+
+
+
+
+
+
+
+
+
