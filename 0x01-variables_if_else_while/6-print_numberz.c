f83d3e4adca237bf26e979078def9a1673c1c8ee
fasil1221 <fasiltassew1@gmail.com>
2022-12-09 13:48:12 -0800

    this is  it

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints base 10 single digit number
+ * Return: always 0
+ */
+	int main(void)
+{
+	char num;
+
+	for (num = 0; num < 10; num++)
+	putchar((num % 10) + '0');
+	putchar("\n")
+	return (0);
+}
