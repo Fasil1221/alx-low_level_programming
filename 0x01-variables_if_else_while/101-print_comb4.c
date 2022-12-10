2912d1160d1ef7052a6de9bf24f3af6954ef8bb6
fasil1221 <fasiltassew1@gmail.com>
2022-12-10 10:27:18 -0800

    thi is it

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,31 +11,31 @@ int main(void)
 	int tens = '0';
 	int hund = '0';
 
-	for (hund = '0'; hund <= '9'; hund++)
+for (hund = '0'; hund <= '9'; hund++)
 {
 	for (tens = '0'; tens <= '9'; tens++)
 
-{
+	{
 
-	for (ones = '0'; ones <= '9'; ones++)
-{
+		for (ones = '0'; ones <= '9'; ones++)
+		{
 
-	if (!((ones == tens) || (tens == hund) || (hund == ones)))
-{
-		if (!((hund > ones) || (tens > ones) || (hund > tens)))
-{
+			if (!((ones == tens) || (tens == hund) || (hund == ones)))
+			{
+				if (!((hund > ones) || (tens > ones) || (hund > tens)))
+				{
 				putchar(hund);
 				putchar(tens);
 				putchar(ones);
 				if (!(ones == '9' && hund == '7' && tens == '8'))
-{
+				{
 				putchar(',');
 				putchar(' ');
-}
-}
-}
-}
-}
+				}
+				}
+			}
+		}
+	}
 }
 					putchar('\n');
 					return (0);
