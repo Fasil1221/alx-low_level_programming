4e655c3a1c248c624190bb334715df733878ea86
fasil1221 <fasiltassew1@gmail.com>
2022-12-10 05:57:44 -0800

    this is my last effort

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - different combo of three digits
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	int ones = '0';
+	int tens = '0';
+	int hund = '0';
+
+	for (hund = '0'; hund <= '9'; hund++)
+{
+		for (tens = '0'; tens <= '9'; tens++)
+
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+					{
+				if (!((ones == tens) || (tens == hund) || (hund > ones) || (tens > ones)))
+				{
+				putchar(hund);
+				putchar(tens);
+				putchar(ones);
+				if (!(ones == '9' && hund == '7' && tens == '8'))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+					} }
+					 }}
+					putchar('\n');
+					return (0);
+
+}
+
+
