ead584f3f4051998c0d80a3e5610f8e6a5c72267
fasil1221 <fasiltassew1@gmail.com>
2022-12-10 04:26:58 -0800

    is this it

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - all combination
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+
+	for (tens = '0'; tens <= '9'; tens++)
+{
+	for (ones = '0'; ones <= '9'; ones++)
+
+{
+	if (!((ones == '9' || (tens > ones))))
+	{
+		putchar(tens);
+		putchar(ones);
+
+		if (!(ones == '9' && tens == '8'))
+		{
+			putchar(',');
+			putchar(' ');
+												}
+												}
+}
+}
+		putchar('\n');
+		return (0);
+}
