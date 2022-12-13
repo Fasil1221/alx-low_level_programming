118d70d766b4b94cfbcc3ab0b3490fdde2373ced
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 03:37:51 -0800

    this is is

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_sign - prints the sign of a number
+*@num: the number up for test
+*Return: 1 0 or -1 or positive zero and negative res
+*/
+int print_sign(int num)
+{
+	if (num > 0)
+	{
+		_putchar('*');
+		return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
+}
