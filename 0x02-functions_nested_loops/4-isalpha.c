72faa2fb1fa574546726acb34a95ac8af661f80a
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 02:53:54 -0800

    this must be it

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - going to cheek if the char is lc and UC
+ * @c: char
+ * Return: 1 if the char is LC or UC, else 0
+ */
+int _isalpha(int c);
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
