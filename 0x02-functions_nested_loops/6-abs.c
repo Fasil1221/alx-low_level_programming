ae4ea092cf9dea538c0cb825e48d5430be662056
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 06:17:55 -0800

    this must be it

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_abs - prints the absolute value of a number
+*@num: the number up for test
+*Return: positive number
+*/
+int _abs(int num);
+{
+	if (num >= 0)
+	{
+		return (num);
+	}
+	else
+	{
+
+	return (-num);
+	}
+}
