439264fff2a892f982ca068f9d797c8bbdc96e75
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 02:19:17 -0800

    this is it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
-/*
- * void print_alphabet_x10 -  prints 10 times the alphabet,
- * followed by, a new line
+/**
+ *print_alphabet_x10 - prints 10 times the alphabet,
  */
 	void print_alphabet_x10(void)
 {
