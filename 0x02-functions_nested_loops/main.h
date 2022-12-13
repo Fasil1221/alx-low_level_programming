dfd3cabfb551109b739830c92a4b3b8ab8877079
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 01:21:57 -0800

    this is it

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+/* i don't think i need 
+ * a comment 
+ * for this header
+ */
+int _putchar(char c);
+void print_alphabet();
+void print_alphabet_x10();
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_time_table(int n);
+void num(int r, int n);
