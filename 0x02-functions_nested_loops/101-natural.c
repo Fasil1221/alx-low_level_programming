ece8eccf44e263096fa626b9ba9a1b1460b9e9a5
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 10:00:52 -0800

    this is it

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,15 +6,16 @@
 int main(void)
 {
 	int n;
+	int m;
 
-	for (n = 0; n <= 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
-			n += n;
+			m += n;
 		}
 	}
-	printf("%d", n);
+	printf("%d", m);
 	printf("\n");
 	return (0);
 }
