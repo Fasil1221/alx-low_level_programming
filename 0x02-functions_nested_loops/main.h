ecd4b3fc6dc8a4d4768570ee190baa11ce15abcc
fasil1221 <fasiltassew1@gmail.com>
2022-12-13 09:17:49 -0800

    this is it

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,5 +14,5 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
+void print_times_table(int n);
 void num(int r, int n);
