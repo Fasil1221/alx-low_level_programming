3b8d7f776ad5f077baa3031f25f0b386e8c4ee0f
fasil1221 <fasiltassew1@gmail.com>
2022-12-15 06:08:21 -0800

    this is my it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Write a function that multiplies two integers
+ * @a: intiger
+ * @b: intiger
+ * Return: a times b
+ */
+int mul(int a, int b)
+{
+	int RLT;
+
+	RLT = a * b;
+	return (RLT);
+}
