5613e09af010e49154aaf7969326d4ff797db447
fasil1221 <fasiltassew1@gmail.com>
2022-12-15 09:01:41 -0800

    this is not

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,9 @@ void more_numbers(void)
 int i;
 int j;
 
-for (i = '0'; i < '10'; i++)
+for (i = '0'; i <= '9'; i++)
 {
-for (j = '0'; j <= '14'; j++)
+for (j = '0'; j <= '9'; j++)
 {
 if (j >= 10)
 {
