bf8228a7fb82006231e6a8eed9796b4fe2f53746
fasil1221 <fasiltassew1@gmail.com>
2022-12-15 06:30:54 -0800

    is this it

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_numbers - Write a function that prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - Write a function that prints the numbers, 
+ * from 0 to 9, followed by a new line
  */
 void print_numbers(void)
 {
