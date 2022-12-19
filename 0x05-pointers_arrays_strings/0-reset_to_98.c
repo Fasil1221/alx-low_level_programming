8f5c6b702138b2c99ecb6bca443fc2f13b0831e4
fasil1221 <fasiltassew1@gmail.com>
2022-12-19 02:03:51 -0800

    this is the easiest ever

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
-/*
- * reset_to_98 - Write a function that takes a pointer to an int as parameter
+/**
+ * reset_to_98- Writes a function that takes a pointer to an int as parameter
  * and updates the value it points to to 98.
  * @n: integer
  * Return: forthis n
