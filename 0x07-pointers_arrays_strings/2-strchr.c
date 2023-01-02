1ea001048bcf239607ea63564d845a414b50904d
fasil1221 <fasiltassew1@gmail.com>
2023-01-02 09:44:33 -0800

    comparing

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,12 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
+	int i = 0;
 
-while (s[i])
-{
-if (s[i] == c)
-return (&s[i]);
-i++;
-}
-return (0);
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
