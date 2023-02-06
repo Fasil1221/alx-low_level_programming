c6bdc8e0fd990fdca7bfc7a2d5f43e4c2713857c
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2023-02-06 10:14:28 -0800

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_endianness - checks if a machine is little or big endian
+ * get_endianness - checks if the machine is little or big endian
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
