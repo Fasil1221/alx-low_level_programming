d2ff5199da28e5cde0120d82a6b0477125b0f504
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2023-02-06 10:13:49 -0800

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of a decimal number
+ * print_binary - prints a binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
