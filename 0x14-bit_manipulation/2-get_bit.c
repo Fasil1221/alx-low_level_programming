9d95367042bea911b215d49fa450333949a15d81
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2023-02-06 10:16:20 -0800

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit - returns the value of a bit at an index in a decimal number
+ * get_bit - returns the value of a bit at an index in the decimal number
  * @n: number to search
  * @index: index of the bit
  *
