7fcce69adcf5e46184369d113db7c4812118b653
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2023-02-06 10:17:19 -0800

    Update 3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a given index to 1
+ * set_bit - sets a bit at the given index to 1
  * @n: pointer to the number to change
  * @index: index of the bit to set to 1
  *
