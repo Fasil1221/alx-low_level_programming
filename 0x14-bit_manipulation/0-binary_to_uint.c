4990a6cbdb37547a23de6d948e53ca5ea3929495
Fasil1221 <117756624+Fasil1221@users.noreply.github.com>
2023-02-06 10:02:34 -0800

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
+ * binary_to_uint - converts the binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
